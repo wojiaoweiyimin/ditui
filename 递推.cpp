12f61f3e197bdaa7b0473f44c029812f72ea55ef
wojiaoweiyimin <91054453+wojiaoweiyimin@users.noreply.github.com>
2021-09-26 07:36:51 +0800

    Add files via upload
    因为有一个for循环所以时间复杂度为O(n)

---
+++ 递推.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int sum,pre,prec; 
+int f(int n){
+	f(1)== 1 ;return pre;
+	f(2)== 2 ;return prec;
+	for(int i=2;i<n;i++){
+		sum==f(2)+f(1);
+		pre==f(2);
+		prec==sum;	
+}
+} 
+int main()
+{
+	cout<<f(3);
+	
+}
